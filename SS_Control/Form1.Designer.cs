da36f1f0ae3e4c49c94809916d9ec97bd430d0e5
Manuito <manuortega@gmail.com>
2015-06-09 19:25:47 +0200

    Inicial

---
+++ SS_Control/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace SS_Control
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(107, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ACTIVAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Estado:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(141, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "DESACTIVADO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Hora actual: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Activar desde:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(88, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Activar hasta:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "HH:mm";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(168, 98);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(58, 20);
+            this.dateTimePicker1.TabIndex = 6;
+            this.dateTimePicker1.Value = new System.DateTime(2015, 1, 19, 0, 0, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.CustomFormat = "HH:mm";
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker2.Location = new System.Drawing.Point(168, 123);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.ShowUpDown = true;
+            this.dateTimePicker2.Size = new System.Drawing.Size(58, 20);
+            this.dateTimePicker2.TabIndex = 7;
+            this.dateTimePicker2.Value = new System.DateTime(2015, 1, 19, 4, 30, 0, 0);
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(165, 64);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 15);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "00:00";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(100, 160);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(107, 50);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(260, 209);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "v. 26/05/15";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Beige;
+            this.ClientSize = new System.Drawing.Size(322, 225);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CONTROL SALVAPANTALLAS";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ SS_Control/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+
+namespace SS_Control
+{
+ 
+    public partial class Form1 : Form
+    {
+        bool auto_change = true;    // Were the dateTimePickers changed manually or automatically?
+        bool active = false;        // Is the counter active?
+        bool itsTime = false;       // Are we actually inside the defined time frame?
+        int countdown = 0;
+
+        // Initialize start and end times
+        DateTime start_time = DateTime.Now;
+        DateTime end_time = DateTime.Now;
+                
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.Visible = false;
+            label6.Text = DateTime.Now.ToShortTimeString();     // Label with current time
+
+            // Checking whether no time has been saved (firt program execution)
+            if (Properties.Settings.Default["ActivateTime"].ToString() != "")
+            {
+                // Return dateTimePicker1 to its saved value
+                string recorded_activate = Properties.Settings.Default["ActivateTime"].ToString();
+                dateTimePicker1.Value = Convert.ToDateTime(recorded_activate);
+
+                start_time = dateTimePicker1.Value;     // Refresh start_time to do the calculations
+            }
+            else
+            {
+                // We save again in value text has not been changed
+                Properties.Settings.Default["ActivateTime"] = dateTimePicker1.Text;
+                Properties.Settings.Default.Save();
+
+                start_time = dateTimePicker1.Value;
+            }
+
+            // Same as above with dateTimePicker2
+            if (Properties.Settings.Default["DeactivateTime"].ToString() != "")
+            {
+                auto_change = true;
+
+                string recorded_deactivate = Properties.Settings.Default["DeactivateTime"].ToString();
+                dateTimePicker2.Value = Convert.ToDateTime(recorded_deactivate);
+
+                end_time = dateTimePicker2.Value;
+            }
+            else
+            {
+                Properties.Settings.Default["DeactivateTime"] = dateTimePicker2.Text;
+                Properties.Settings.Default.Save();
+
+                end_time = dateTimePicker2.Value;
+            }
+
+
+            auto_change = false;
+
+            timer1.Start();
+        }
+
+
+        //********************
+        // Launch Screen Saver
+        //********************
+        [DllImport("User32.dll")]
+        public static extern int PostMessage
+            (IntPtr hWnd,
+            uint Msg,
+            uint wParam,
+            uint lParam);
+
+        public const uint WM_SYSCOMMAND = 0x112;
+        public const uint SC_SCREENSAVE = 0xF140;
+
+        public enum SpecialHandles
+        {
+            HWND_DESKTOP = 0x0,
+            HWND_BROADCAST = 0xFFFF
+        }
+
+        public static void TurnOnScreenSaver()
+        {
+            PostMessage(
+                new IntPtr((int)SpecialHandles.HWND_BROADCAST),
+                WM_SYSCOMMAND,
+                SC_SCREENSAVE,
+                0);
+        }
+        //********************
+        //********************
+        //********************
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Enable or disable "active"
+            if (active == false)
+            {
+                // If we did NOT activate when inside the time frame, just activate
+                if (itsTime == false)
+                {
+                    label2.Text = "ACTIVADO";
+                    label2.ForeColor = Color.Green;
+                    button1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    button1.Text = "DESACTIVAR";
+                    active = true;
+                }
+                // But if we are inside the time frame, make a countdown!
+                else
+                {
+                    button1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                    button1.Text = "LAUNCH IN 4";
+                    button1.ForeColor = Color.Green;
+                    button1.Enabled = false;
+                    countdown++;
+                }
+            }
+            else
+            {
+                label2.Text = "DESACTIVADO";
+                label2.ForeColor = Color.Red;
+                button1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                button1.Text = "ACTIVAR";
+                active = false;
+                timer1.Interval = 1000;     // Back to normal timer interval
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool timeChecks = false;
+            
+            DateTime local_time = DateTime.Now;
+
+            ScreenSaver.SetScreenSaverTimeout(900);   // Change screensaver timeout to 900 seconds
+
+            label6.Text = local_time.ToShortTimeString();        // Updates the current time
+
+
+            // Checks if time checks with adding 24 hours in case end_time goes beyond midnight
+            if (end_time > start_time)
+            {
+                if ((local_time > start_time) && (local_time < end_time))
+                {
+                    timeChecks = true;
+                }
+            }
+            else if ((local_time < start_time) && (local_time.AddDays(1) < end_time.AddDays(1)))
+            {
+                timeChecks = true;
+            }
+            else if ((local_time > start_time) && (local_time.AddDays(1) > end_time.AddDays(1)))
+            {
+                timeChecks = true;
+            }
+            
+
+            // Then we proceed if time checks
+            if (timeChecks)
+            {
+                itsTime = true;
+                label6.ForeColor = Color.Green;
+                if (active)
+                {
+                    // We only want to launch the screensaver once (if it isn't running yet!)
+                    if (ScreenSaver.GetScreenSaverRunning() == false)
+                    {
+                        timer1.Interval = 300000;    // Give some time to move mouse and deactivate screen saver
+                        TurnOnScreenSaver();        // Launch!
+                        label2.Text = "ACTIVADO";
+                        button1.Enabled = true;
+                    }
+                }
+            }
+            else
+            {
+                itsTime = false;
+                label6.ForeColor = Color.Black;
+                if (active)
+                {
+                    // Just try to kill if it's running, otherwise do nothing
+                    if (ScreenSaver.GetScreenSaverRunning() == true)
+                    {
+                        ScreenSaver.KillScreenSaver();      // Deactivate screen saver
+                        timer1.Interval = 1000;             // Back to normal timer interval
+                        label2.Text = "DESACTIVADO";
+                    }
+                }
+            }
+
+            // Countdown in button
+            if (countdown == 4)
+            {
+                button1.Text = "DESACTIVAR";
+                button1.ForeColor = Color.Black;
+                countdown = 0;
+            }
+            if (countdown == 3)
+            {
+                button1.Text = "LAUNCH IN 1";
+                button1.ForeColor = Color.Green;
+                countdown++;
+                active = true;
+            }
+            if (countdown == 2)
+            {
+                button1.Text = "LAUNCH IN 2";
+                button1.ForeColor = Color.Green;
+                countdown++;
+            }
+            if (countdown == 1)
+            {
+                button1.Text = "LAUNCH IN 3";
+                button1.ForeColor = Color.Green;
+                countdown++;
+            }
+        
+            if (itsTime && active)
+            {
+                pictureBox1.Visible = true;
+            }
+            else
+            {
+                pictureBox1.Visible = false;
+            }
+        }
+
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            // If the change is performed manually we do not want to call this function
+            if (auto_change == false)
+            {
+                // Save value in dateTimePicker1
+                Properties.Settings.Default["ActivateTime"] = dateTimePicker1.Text;
+                Properties.Settings.Default.Save();
+               
+                start_time = dateTimePicker1.Value;      // Also update start_time with the new value
+            }
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            if (auto_change == false)
+            {
+                // Save value in dateTimePicker2
+                Properties.Settings.Default["DeactivateTime"] = dateTimePicker2.Text;
+                Properties.Settings.Default.Save();
+
+                end_time = dateTimePicker2.Value;       // Also update end_time with the new value
+            }
+        }
+    }
+}
